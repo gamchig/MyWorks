188a6f54fbca9e4101a75897cfeee28c682c8463
NdtFoxy <nikita12764586@gmail.com>
2022-01-11 17:19:46 +0200

    first commit

---
+++ lesson29/script.js
@@ -0,0 +1,23 @@
+// let x = 10;
+// console.log(x);
+
+// let y = 5;
+// console.log(y);
+
+// let word = 'привет';
+// console.log(word)
+// let r = 1;
+// alert(r)
+//  r = prompt(1234)
+//  alert(r)
+
+//  let userAge = prompt('Сколько лет?')
+//  let userName = prompt('Ваше имя?')
+//  let userWork = prompt('Место работы или номер школы?')
+//  console.log( 'Мне ' + userAge + ' Меня зовут '+ userName + ' Мое место занятости ' + userWork);
+
+let  firstNumber =  prompt('Введите первое число')
+let num = Number(firstNumber)
+let  secondNumber = prompt('Введите второе число')
+let secondnum = Number(secondNumber)
+console.log(num + secondnum)

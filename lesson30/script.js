13284422ea199c98b06dc2c971da9c6830a70eff
NdtFoxy <nikita12764586@gmail.com>
2022-01-14 18:00:23 +0200

    lesson30

---
+++ lesson30/script.js
@@ -0,0 +1,43 @@
+// // console.log(-1/0)
+// let x = 1; 
+// console.log(x, typeof(x))
+// x = 7;
+// a = 5
+// z = 10 
+// let y = 7*x**6+Math.sqrt(a)+x**3- z/5+a**x
+// Math.pow(x, 6)
+// Math.sqrt(a)
+// Math.pow(x, 3)
+// Math.pow(a, x)
+// console.log(y)
+
+// Уровнения
+//     let x = prompt("Введите число");
+//     if (x > 0){
+//          console.log(2*Math.sin(x))
+//     }
+//     else if( x <= 0)
+//     console.log(6 - x)
+
+
+// let x = prompt("Введите число");
+// if ( x < -2 || x > 2){
+//     console.log(2 * x)
+// } 
+// else{
+//     console.log(-3*x)
+// }
+// let x = prompt("Введите число");
+// if (x <= 0){
+//     console.log(-x)
+// }
+
+// let x = prompt("Введите число");
+// if (0 < x < 2){
+//     console.log(x**2)
+// }
+
+// let x = prompt("Введите число");
+//  if ( x => 2){
+//     console.log(4)
+//  }
